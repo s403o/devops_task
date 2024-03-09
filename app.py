953dcaf0bfed9dfa07e08b9d27be23941a8a302d
Eslam Adel <eadel@lucidya.com>
2024-03-09 04:05:24 +0200

    add k8s mainfest - add sigterm & logger

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, render_template
 from pymongo import MongoClient
 from datetime import datetime
-import os
+import os, logging, signal
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+
+# Set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 mongo_uri = os.environ.get("MONGO_URI")
 # Connect to MongoDB
@@ -37,5 +41,14 @@ def index():
     return render_template("index.html")
 
 
+def handle_exit(sig, frame):
+    raise(SystemExit)
+
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    signal.signal(signal.SIGINT, handle_exit)
+    try:
+        logger.info("Starting the application...")
+        app.run(debug=False, host="0.0.0.0", port=5000)
+    except (KeyboardInterrupt, SystemExit) as k:
+        logger.exception("Shutting down requested. Exiting...")
+    
